298fa28ee5d13eeafc93940639dc0ca2159c17aa
LauraKirby <laurakirby26@gmail.com>
2015-07-27 15:04:31 -0700

    add shoulda-matchers, add test for User models, add shoulda-matchers gem

--- hangout/spec/models/user_spec.rb
+++ hangout/spec/models/user_spec.rb
@@ -4,17 +4,27 @@ describe 'User' do
 
   subject(:user) { User.create(
                             first_name: "Fifi",
-                            image_url: "me.png",
                             email: "example.com",
-                            pwd: "secret"
+                            img_url: "me.png",
+                            password: "secret"
                             )}
 
-    [:first_name, :image_url, :email, :pwd].each do |prop|
+    [:first_name, :img_url, :email, :password].each do |prop|
         it {is_expected.to respond_to prop}
+
     end
 
-
     #This tests for uniqueness in the User model
-    it { is_expected.to validate_uniqueness_of :email}
+    [:email].each do |prop|
+    	it { is_expected.to validate_presence_of :email}
+    end 
 
 end
+
+
+      # t.string :first_name
+      # t.string :email
+      # t.string :img_url
+      # t.string :address
+      # t.string :password
+      # t.string :password_digest
