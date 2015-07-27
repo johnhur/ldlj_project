e0867664d17d484ee9a369b6df081d28c56f8ab5
LauraKirby <laurakirby26@gmail.com>
2015-07-27 14:33:13 -0700

    include rspec install files

--- hangout/spec/models/user_spec.rb
+++ hangout/spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'User' do
+
+  subject(:user) { User.create(
+                            first_name: "Fifi",
+                            image_url: "me.png",
+                            email: "example.com",
+                            pwd: "secret"
+                            )}
+
+    [:first_name, :image_url, :email, :pwd].each do |prop|
+        it {is_expected.to respond_to prop}
+    end
+
+
+    #This tests for uniqueness in the User model
+    it { is_expected.to validate_uniqueness_of :email}
+
 end
