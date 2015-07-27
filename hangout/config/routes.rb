4fcb12be02b4170a4477108c14732ee5f1c31fd3
Lorien <LorienMCS@gmail.com>
2015-07-27 16:38:43 -0700

    add Google map, geolocation, Handlebars, and weather

--- hangout/config/routes.rb
+++ hangout/config/routes.rb
@@ -4,7 +4,33 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#login", as: 'login'
   post '/login', to: "sessions#attempt_login"
   delete '/logout', to: "sessions#logout", as: "logout"
-  
+
+
+	# root 'users#index'
+
+	get '/search', to: 'places#search'
+
+
   resources :users
   resources :places
 end
+
+# Prefix Verb   URI Pattern                Controller#Action
+#       root GET    /                          places#index
+#     search GET    /search(.:format)          places#search
+#      users GET    /users(.:format)           users#index
+#            POST   /users(.:format)           users#create
+#   new_user GET    /users/new(.:format)       users#new
+#  edit_user GET    /users/:id/edit(.:format)  users#edit
+#       user GET    /users/:id(.:format)       users#show
+#            PATCH  /users/:id(.:format)       users#update
+#            PUT    /users/:id(.:format)       users#update
+#            DELETE /users/:id(.:format)       users#destroy
+#     places GET    /places(.:format)          places#index
+#            POST   /places(.:format)          places#create
+#  new_place GET    /places/new(.:format)      places#new
+# edit_place GET    /places/:id/edit(.:format) places#edit
+#      place GET    /places/:id(.:format)      places#show
+#            PATCH  /places/:id(.:format)      places#update
+#            PUT    /places/:id(.:format)      places#update
+#            DELETE /places/:id(.:format)      places#destroy
