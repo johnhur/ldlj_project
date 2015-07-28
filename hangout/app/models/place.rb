d38b379025ae2da72a1454f45394b84eefce5d9a
LauraKirby <laurakirby26@gmail.com>
2015-07-28 10:06:30 -0700

    testing yelp api

--- hangout/app/models/place.rb
+++ hangout/app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ActiveRecord::Base
-
+before_action :self.new_place
+	
 	validates :business_name, presence: true
 
 	has_many :favorites, dependent: :destroy
@@ -8,11 +9,16 @@ class Place < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	has_many :users, through: :comments
 
-	def self.new_place(location, business)
-		@new_yelp = Yelp::Client.new({ consumer_key: Rails.application.secrets[:YELP_CONSUMER_KEY],
-                            consumer_secret: Rails.application.secrets[:YELP_CONSUMER_SECRET],
-                            token: Rails.application.secrets[:YELP_TOKEN],
-                            token_secret: Rails.application.secrets[:YELP_TOKEN_SECRET]
-                         })
+	def new_place_search (location, business, num)
+		@new_yelp_search.search(location, business, num)
+	end 
+
+
+	def self.new_place
+		# @new_yelp = Yelp::Client.new({ consumer_key: Rails.application.secrets[:YELP_CONSUMER_KEY],
+  #                           consumer_secret: Rails.application.secrets[:YELP_CONSUMER_SECRET],
+  #                           token: Rails.application.secrets[:YELP_TOKEN],
+  #                           token_secret: Rails.application.secrets[:YELP_TOKEN_SECRET]
+  #                        })
 	end 
 end
