ea6e802909cfe297c3f8448ab8e0830bbf7f4760
LauraKirby <laurakirby26@gmail.com>
2015-07-28 10:37:23 -0700

    fix yelp api bug, demo on /places now working

--- hangout/app/controllers/places_controller.rb
+++ hangout/app/controllers/places_controller.rb
@@ -1,25 +1,10 @@
 class PlacesController < ApplicationController
 
 	def index #create a search
-		parameters = {term: 'food', limit: 10}
-		client = Yelp::Client.new({ consumer_key: Rails.application.secrets[:YELP_CONSUMER_KEY],
-                            consumer_secret: Rails.application.secrets[:YELP_CONSUMER_SECRET],
-                            token: Rails.application.secrets[:YELP_TOKEN],
-                            token_secret: Rails.application.secrets[:YELP_TOKEN_SECRET]
-                         })
-		new_search =  client.search('San Francisco', parameters)
-		@result = new_search.businesses[0].name
-		binding.pry
+		new_search =  Yelp.client.search('San Francisco', {term: 'food'})
+		@results = new_search.businesses[0].name
+	 
 	end
 
 
 end
-
-# places GET    /places(.:format)          places#index
-#            POST   /places(.:format)          places#create
-#  new_place GET    /places/new(.:format)      places#new
-# edit_place GET    /places/:id/edit(.:format) places#edit
-#      place GET    /places/:id(.:format)      places#show
-#            PATCH  /places/:id(.:format)      places#update
-#            PUT    /places/:id(.:format)      places#update
-#            DELETE /places/:id(.:format)      places#destroy
--- hangout/config/initializers/yelp.rb
+++ hangout/config/initializers/yelp.rb
@@ -4,24 +4,3 @@ Yelp.client.configure do |config|
   config.token = Rails.application.secrets[:YELP_TOKEN]
   config.token_secret = Rails.application.secrets[:YELP_TOKEN_SECRET]
 end
-
-# Yelp.client.configure do |config|
-#   config.consumer_key = ENV['YELP_CONSUMER_KEY']
-#   config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
-#   config.token = ENV['YELP_TOKEN']
-#   config.token_secret = ENV['YELP_TOKEN_SECRET']
-# end
-
-# Yelp.client.configure do |config|
-#   config.consumer_key = YOUR_CONSUMER_KEY
-#   config.consumer_secret = YOUR_CONSUMER_SECRET
-#   config.token = YOUR_TOKEN
-#   config.token_secret = YOUR_TOKEN_SECRET
-# end
-
-
-# client = Yelp::Client.new({ consumer_key: Rails.application.secrets[:YELP_CONSUMER_KEY],
-#                             consumer_secret: Rails.application.secrets[:YELP_CONSUMER_SECRET],
-#                             token: Rails.application.secrets[:YELP_TOKEN],
-#                             token_secret: Rails.application.secrets[:YELP_TOKEN_SECRET]
-#                          })
