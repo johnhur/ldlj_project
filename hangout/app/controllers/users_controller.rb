abee682ca61c56dd13b7e3af2c3b16997b30ba97
Daphne Watson <daphne.renee.laurels@gmail.com>
2015-07-28 13:21:59 -0700

    adding in imageuploader

--- hangout/app/controllers/users_controller.rb
+++ hangout/app/controllers/users_controller.rb
@@ -23,10 +23,12 @@ class UsersController < ApplicationController
   def show
   end
 
+# added avatar info into the def user_params
   private
   def user_params
   	params.require(:user).permit(
   		:first_name,
+      :avatar,
   		:email,
   		:img_url,
   		:address,
--- hangout/app/models/user.rb
+++ hangout/app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
 	validates :email, presence: true
 	validates :password, presence: true
 	# we will add in additional validations later on
+	
+	# added avatar information 
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"}, :default_url => "/images/:style/missing.png" 
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*Z/
 
 	has_many :favorites, dependent: :destroy
 	has_many :places, through: :favorites
