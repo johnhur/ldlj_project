515c7e81b9457b03fdb4e57585d0185c1837b8ff
Daphne Watson <daphne.renee.laurels@gmail.com>
2015-07-29 10:15:35 -0700

    working with sass on signup page

--- hangout/db/schema.rb
+++ hangout/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150728182307) do
-=======
-ActiveRecord::Schema.define(version: 20150728004549) do
->>>>>>> 4c025749094ac01d5bb30336c1b3c997a0421f9b
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,5 +63,4 @@ ActiveRecord::Schema.define(version: 20150728004549) do
   add_foreign_key "comments", "users"
   add_foreign_key "favorites", "places"
   add_foreign_key "favorites", "users"
-end
 end
