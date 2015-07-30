9c9326cf579cb8a0f0356b9829c817abdc2a2a57
johnhur <johnjhur@gmail.com>
2015-07-30 15:20:16 -0700

    captured lat & lng through user login page

--- hangout/app/controllers/sessions_controller.rb
+++ hangout/app/controllers/sessions_controller.rb
@@ -1,12 +1,16 @@
 class SessionsController < ApplicationController
 
   def login
+
   end
 
   def attempt_login
   	if params[:email].present? && params[:password].present?
   	  found_user = User.where(email: params[:email]).first
+
   	  if found_user && found_user.authenticate(params[:password])
+        found_user.update(lat: params[:user_lat])
+        found_user.update(lng: params[:user_lng])
         session[:user_id] = nil
   	    session[:user_id] = found_user.id
   	    redirect_to users_path
@@ -21,9 +25,8 @@ class SessionsController < ApplicationController
   end
   
   def logout
-    
-    # current_user.lat = nil
-    # current_user.lng = nil
+    current_user.update(lat: nil)
+    current_user.update(lng: nil)
   	session[:user_id] = nil
   	flash[:notice] = "Logged out"
   	redirect_to login_path
