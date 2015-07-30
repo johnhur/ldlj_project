6035394c7291d2c93a57e247ca40fb433c9a8c8d
johnhur <johnjhur@gmail.com>
2015-07-30 11:37:30 -0700

    midpoint data on map, needs refactoring

--- hangout/app/controllers/places_controller.rb
+++ hangout/app/controllers/places_controller.rb
@@ -1,18 +1,27 @@
 class PlacesController < ApplicationController
 
-	def index 	 
+	def index
 	end
 
+  # search is on same page as results, so only need one method
   def results
-    # lat lng from user.js AJAX get request
+    # lat, lng, and term from user.js AJAX get request
     @lat = params[:lat]
     @long = params[:lng]
     term = params[:term]
-    yelp_params = { term: term, limit: 2}
+    yelp_params = { term: term, limit: 2, offset: 5, sort: 1}
     coordinates = { latitude: @lat, longitude: @long }
     new_search = Yelp.client.search_by_coordinates(coordinates, yelp_params)
-
-    render json: "this is where the Yelp search results will be"
+    # TODO - refactor into a separate function
+    new_search.businesses.each do |business|
+    	  result_name = business.name
+    	  result_address = business.location.address
+    	  result_lat = business.location.coordinate.latitude
+    	  result_long = business.location.coordinate.longitude
+    	  # result_review = business.review_count
+    	  # result_rating = business.rating
+        end 
+    render json: new_search
   end
 
 end
