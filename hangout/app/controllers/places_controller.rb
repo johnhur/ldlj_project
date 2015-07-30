7265c05c7ead26bc66e6c79a06a731f11d6d5a5f
LauraKirby <laurakirby26@gmail.com>
2015-07-29 23:51:03 -0700

    store lat and long from place returned in result action - YAY

--- hangout/app/controllers/places_controller.rb
+++ hangout/app/controllers/places_controller.rb
@@ -8,10 +8,18 @@ class PlacesController < ApplicationController
     @lat = params[:lat]
     @long = params[:lng]
     term = params[:term]
-    yelp_params = { term: term, limit: 2}
+    yelp_params = { term: term, limit: 2, offset: 5, sort: 1}
     coordinates = { latitude: @lat, longitude: @long }
     new_search = Yelp.client.search_by_coordinates(coordinates, yelp_params)
-
+    new_search.businesses.each do |business|
+    	  result_name = business.name
+    	  result_address = business.location.address
+    	  result_lat = business.location.coordinate.latitude
+    	  result_long = business.location.coordinate.longitude
+    	  # result_review = business.review_count
+    	  # result_rating = business.rating
+        end 
+        
     render json: "this is where the Yelp search results will be"
   end
 
